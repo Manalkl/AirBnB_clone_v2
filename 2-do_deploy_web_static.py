d61e7fa927f3fd54f6385e585d3484d4bec1c269
Manalkl <manel.kl.mk@gmail.com>
2023-10-11 14:10:10 -0700

    up

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,48 +1,41 @@
 #!/usr/bin/python3
-# Fabfile to distribute an archive to a web server.
-import os.path
-from fabric.api import env
-from fabric.api import put
-from fabric.api import run
+"""Fabric script that generates a .tgz archive"""
+from fabric.api import local, run, put, env
+from datetime import datetime
+from os import path
 
-env.hosts = ["54.160.85.72", "35.175.132.106"]
+env.hosts = ["100.25.2.123"]
+
+
+def do_pack():
+    """All files in the folder web_static"""
+    local("mkdir -p ./versions")
+    current_time = datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")
+    res = local(f'tar -cvzf versions/web_static_{current_time}.tgz web_static')
+    if res.failed:
+        return None
+    else:
+        print(res)
+        return res
 
 
 def do_deploy(archive_path):
-    """Distributes an archive to a web server.
-    Args:
-        archive_path (str): The path of the archive to distribute.
-    Returns:
-        If the file doesn't exist at archive_path or an error occurs - False.
-        Otherwise - True.
-    """
-    if os.path.isfile(archive_path) is False:
+    """ distributes an archive to your web servers"""
+    if not path.exists(archive_path):
         return False
-    file = archive_path.split("/")[-1]
-    name = file.split(".")[0]
-
-    if put(archive_path, "/tmp/{}".format(file)).failed is True:
+    try:
+        put(archive_path, "/tmp/")
+        print ("Hello")
+        file_name = path.basename(archive_path)
+        target_dir_name = f"/data/web_static/releases/{file_name[:-4]}"
+        run(f"sudo mkdir -p {target_dir_name}")
+        run(f"sudo tar -xzf /tmp/{file_name} -C {target_dir_name}")
+        run(f"sudo rm /tmp/{file_name}")
+        first = f'/data/web_static/releases/{file_name[:-4]}/web_static/*'
+        run(f'sudo mv {first}  /data/web_static/releases/{file_name[:-4]}/')
+        run(f'sudo rm -rf {first}')
+        run("sudo rm -rf /data/web_static/current")
+        run(f"sudo ln -s {target_dir_name} /data/web_static/current")
+        return True
+    except Exception:
         return False
-    if run("rm -rf /data/web_static/releases/{}/".
-           format(name)).failed is True:
-        return False
-    if run("mkdir -p /data/web_static/releases/{}/".
-           format(name)).failed is True:
-        return False
-    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
-           format(file, name)).failed is True:
-        return False
-    if run("rm /tmp/{}".format(file)).failed is True:
-        return False
-    if run("mv /data/web_static/releases/{}/web_static/* "
-           "/data/web_static/releases/{}/".format(name, name)).failed is True:
-        return False
-    if run("rm -rf /data/web_static/releases/{}/web_static".
-           format(name)).failed is True:
-        return False
-    if run("rm -rf /data/web_static/current").failed is True:
-        return False
-    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
-           format(name)).failed is True:
-        return False
-    return True
